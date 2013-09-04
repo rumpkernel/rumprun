ace8dbcc26f78ee64d48a046baba4d17b4bc3975
Antti Kantee <pooka@iki.fi>
2013-09-04 13:43:23 +0300

    Make some stubs return NULL so as to signal the caller.

--- libc_stubs.c
+++ libc_stubs.c
@@ -7,20 +7,31 @@
 	printk("STUB ``%s'' called\n", #name);	\
 	return 2;}
 
+#define STUBNULL(name)				\
+  void *name(void); void *name(void) {		\
+	static int done = 0;			\
+	if (done) return NULL; done = 1;	\
+	printk("STUB ``%s'' called\n", #name);	\
+	return NULL;}
+
 STUB(__clock_gettime50);
 STUB(__nanosleep50);
 STUB(__setitimer50);
 STUB(__sigaction14);
 STUB(__sigprocmask14);
-STUB(_citrus_LC_CTYPE_setlocale);
-STUB(_citrus_LC_MESSAGES_setlocale);
-STUB(_citrus_LC_MONETARY_setlocale);
-STUB(_citrus_LC_NUMERIC_setlocale);
-STUB(_citrus_LC_TIME_setlocale);
-STUB(_citrus_ctype_default);
-STUB(_citrus_lookup_simple);
+
 STUB(_exit);
 STUB(_lwp_kill);
 STUB(_lwp_self);
 STUB(_mmap);
 STUB(munmap);
+STUB(__wait450);
+STUB(__fork);
+
+STUBNULL(_citrus_LC_CTYPE_setlocale);
+STUBNULL(_citrus_LC_MESSAGES_setlocale);
+STUBNULL(_citrus_LC_MONETARY_setlocale);
+STUBNULL(_citrus_LC_NUMERIC_setlocale);
+STUBNULL(_citrus_LC_TIME_setlocale);
+STUBNULL(_citrus_ctype_default);
+STUBNULL(_citrus_lookup_simple);
