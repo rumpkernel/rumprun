282a10cbd69e920e7385f4bae85f2f473a7e334f
Antti Kantee <pooka@iki.fi>
2013-09-07 15:50:20 +0300

    __MINIOS__ seems to be relevant for newlib ==> g/c

--- Config.mk
+++ Config.mk
@@ -47,6 +47,5 @@ include $(MINI-OS_ROOT)/$(TARGET_ARCH_DIR)/arch.mk
 extra_incl := $(foreach dir,$(EXTRA_INC),-isystem $(MINI-OS_ROOT)/include/$(dir))
 
 DEF_CPPFLAGS += -isystem $(MINI-OS_ROOT)/include
-DEF_CPPFLAGS += -D__MINIOS__
 
 DEF_CPPFLAGS += -DSCHED_DEBUG
