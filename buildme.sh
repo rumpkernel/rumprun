a6260c2192df030959915b794660e0c4e09970d4
Antti Kantee <pooka@iki.fi>
2014-08-11 17:59:23 +0000

    first attempt at a build script for "userspace"
    (not tested in its present form, though I derived this commit from
    the script I used in my experiments)

---
+++ buildme.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Script to build "userspace".  Note, not "consumer-grade" yet!
+#
+# first run buildrump.sh with:
+#   ./buildrump.sh -s ${APPSTACK_SRC} -V MKPIC=no -k -N -H fullbuild
+
+APPSTACK_SRC= /home/pooka/src/src-netbsd-appstack
+BUILDRUMP_SH= /home/pooka/src/buildrump.sh
+
+MORELIBS="external/bsd/flex/lib
+        crypto/external/bsd/openssl/lib/libcrypto
+        crypto/external/bsd/openssl/lib/libdes
+        crypto/external/bsd/openssl/lib/libssl
+        external/bsd/libpcap/lib"
+LIBS="${APPSTACK_SRC}/lib/lib*"
+for lib in ${MORELIBS}; do
+        LIBS="${LIBS} ${APPSTACK_SRC}/${lib}"
+done
+
+RUMPMAKE=${BUILDRUMP_SH}/obj/tooldir/rumpmake
+
+. ${BUILDRUMP_SH}/subr.sh
+
+usermtree ${BUILDRUMP_SH}/rump
+userincludes ${RUMPMAKE} ${APPSTACK_SRC} ${LIBS}
+
+for lib in ${LIBS}; do
+        makeuserlib ${RUMPMAKE} ${lib}
+done
+
