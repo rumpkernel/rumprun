cb9f1acd21e1082fb126948051869bfab19dd09b
Antti Kantee <pooka@iki.fi>
2014-08-21 22:28:51 +0000

    fix test

--- rumppci.c
+++ rumppci.c
@@ -59,7 +59,7 @@ void *
 rumpcomp_pci_irq_establish(unsigned cookie, int (*handler)(void *), void *data)
 {
 
-	if (bmk_isr_netinit(handler, data, intrs[cookie]))
+	if (bmk_isr_netinit(handler, data, intrs[cookie]) == 0)
 		return &intrs[cookie];
 	else
 		return NULL;
