dfce49e7afbff6362714c8511f020d673f67483b
Antti Kantee <pooka@iki.fi>
2014-11-18 01:00:57 +0000

    support \r

--- kernel.c
+++ kernel.c
@@ -106,16 +106,21 @@ bmk_cons_putc(int c)
 	static int cons_x;
 	static int cons_y;
 	int x;
+	int doclear = 0;
 
 	if (c == '\n') {
 		cons_x = 0;
 		cons_y++;
+		doclear = 1;
+	} else if (c == '\r') {
+		cons_x = 0;
 	} else {
 		cons_putat(c, cons_x++, cons_y);
 	}
 	if (cons_x == CONS_WIDTH) {
 		cons_x = 0;
 		cons_y++;
+		doclear = 1;
 	}
 	if (cons_y == CONS_HEIGHT) {
 		cons_y--;
@@ -123,8 +128,7 @@ bmk_cons_putc(int c)
 		for (x = 0; x < (CONS_HEIGHT-1)*CONS_WIDTH; x++)
 			cons_buf[x] = cons_buf[x+CONS_WIDTH];
 	}
-	if (cons_x == 0) {
-		/* if we're starting a new row, clear it */
+	if (doclear) {
 		for (x = 0; x < CONS_WIDTH; x++)
 			cons_putat(' ', x, cons_y);
 	}
