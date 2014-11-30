66203d7869af7ae3ed9e73c5aa82b70a16c0a280
Antti Kantee <pooka@iki.fi>
2014-11-30 22:51:18 +0000

    Overriding probe of RUMP_CURLWP is no longer necessary in -k builds

--- buildxen.sh
+++ buildxen.sh
@@ -30,7 +30,7 @@ done
 # build tools
 ./buildrump.sh/buildrump.sh -${BUILDXEN_QUIET:-q} ${STDJ} -k \
     -V MKPIC=no -s rumpsrc -T rumptools -o rumpobj -N \
-    -V RUMP_CURLWP=hypercall -V RUMP_KERNEL_IS_LIBC=1 tools
+    -V RUMP_KERNEL_IS_LIBC=1 tools
 # FIXME to be able to specify this as part of previous cmdline
 echo 'CPPFLAGS+=-DMAXPHYS=32768' >> rumptools/mk.conf
 
