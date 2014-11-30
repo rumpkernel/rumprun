550c2e4815553884ecb6934b780394bc02ba8b62
Antti Kantee <pooka@iki.fi>
2014-11-30 11:15:16 +0000

    if requested, write test results to the disk image

--- app.c
+++ app.c
@@ -88,6 +88,7 @@ nettest(void)
 	printf("[omitting rest ...]\n");
 }
 
+#define TESTMAGIC "PLEASE WRITE ON THIS IMAGE"
 static void
 disktest(void)
 {
@@ -96,7 +97,7 @@ disktest(void)
 	MD5_CTX md5ctx;
 	int fd, i;
 
-	fd = open("/dev/rld0d", O_RDONLY);
+	fd = open("/dev/rld0d", O_RDWR);
 	/* assume it's not present instead of failing */
 	if (fd == -1 && (errno == ENOENT || errno == ENXIO)) {
 		printf("no disk device available.  skipping test\n");
@@ -110,11 +111,28 @@ disktest(void)
 		MD5_Update(&md5ctx, buf, sizeof(buf));
 	MD5_Final(md5sum, &md5ctx);
 
-	printf("0x");
 	for (i = 0; i < sizeof(md5sum); i++) {
 		printf("%x", md5sum[i]);
 	}
 	printf("\n");
+
+	if (pread(fd, buf, sizeof(buf), 0) != sizeof(buf))
+		err(1, "read sector 0");
+
+	/* check if writing the test output was requested */
+	if (strncmp(buf, TESTMAGIC, sizeof(TESTMAGIC)-1) != 0)
+		return;
+
+	printf("writing test results onto image\n");
+	memset(buf, 0, sizeof(buf));
+	strlcat(buf, "OK\n", sizeof(buf));
+	for (i = 0; i < sizeof(md5sum); i++) {
+		char tmp[4];
+		snprintf(tmp, sizeof(tmp), "%02x", md5sum[i]);
+		strlcat(buf, tmp, sizeof(buf));
+	}
+	strlcat(buf, "\n", sizeof(buf));
+	pwrite(fd, buf, sizeof(buf), 0);
 }
 
 /*
