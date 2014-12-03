41fe2900e22766e9e7f6861cfae57a638a325d47
Antti Kantee <pooka@iki.fi>
2014-12-03 15:29:36 +0000

    add beginning of ARM support
    (well, not really, but at least now it's more obvious which pieces
    need to be filled for ARM support)

---
+++ include/arch/arm/md.h
@@ -0,0 +1,27 @@
+#ifndef _BMK_ARCH_ARM_MD_H_
+#define _BMK_ARCH_ARM_MD_H_
+
+#include <bmk/kernel.h>
+
+static inline void
+splhigh(void)
+{
+
+	/* XXX TODO */
+}
+
+static inline void
+spl0(void)
+{
+
+	/* XXX TODO */
+}
+
+static inline void
+hlt(void)
+{
+
+	/* XXX TODO */
+}
+
+#endif /* _BMK..._H_ */
