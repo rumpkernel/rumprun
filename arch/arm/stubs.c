0d753432d6351482191fa3e48ad7223d0ff22852
Antti Kantee <pooka@iki.fi>
2014-12-03 15:53:18 +0000

    add "real" stubs

--- arch/arm/stubs.c
+++ arch/arm/stubs.c
@@ -1,1 +1,10 @@
-int foo;
+#define STUB(name) int name(void); int name(void) {for(;;);}
+
+STUB(bmk_cpu_init);
+STUB(bmk_cpu_intr_init);
+STUB(bmk_cpu_intr_ack);
+STUB(bmk_cpu_clock_now);
+STUB(bmk_cpu_nanohlt);
+STUB(bmk_cpu_sched_create);
+STUB(bmk_cpu_sched_bouncer);
+STUB(bmk_cpu_sched_switch);
