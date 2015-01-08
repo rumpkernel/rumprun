ef6ec09f57d38e89e5f4343322ce497eed1d0728
Antti Kantee <pooka@iki.fi>
2015-01-08 18:28:57 +0000

    sysproxy stubs are no longer required

--- undefs.c
+++ undefs.c
@@ -27,17 +27,6 @@ NOTHING(rumpuser_bio)
 NOTHING(rumpuser_iovread)
 NOTHING(rumpuser_iovwrite)
 
-#ifndef RUMP_SYSPROXY
-NOTHING(rumpuser_sp_anonmmap)
-NOTHING(rumpuser_sp_copyin)
-NOTHING(rumpuser_sp_copyinstr)
-NOTHING(rumpuser_sp_copyout)
-NOTHING(rumpuser_sp_copyoutstr)
-NOTHING(rumpuser_sp_fini)
-NOTHING(rumpuser_sp_init)
-NOTHING(rumpuser_sp_raise)
-#endif
-
 NOTHING(rumpuser_unmap)
 
 /* libc */
