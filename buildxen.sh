35fed293a5e2745c18033534f7af7014935a3c33
Martin Lucina <martin@lucina.net>
2015-02-16 17:07:00 +0100

    buildxen.sh: Fail clearly if intermediate steps fail
    Signed-off-by: Martin Lucina <martin@lucina.net>

--- buildxen.sh
+++ buildxen.sh
@@ -23,7 +23,10 @@ RUMPMAKE="$(pwd)/rumptools/rumpmake"
 # Build librumprun_unwind only if we have C++ support
 BUILDRUMP_CXX="$(${RUMPMAKE} -f rumptools/mk.conf -V '${_BUILDRUMP_CXX}')"
 if [ "${BUILDRUMP_CXX}" = "yes" ]; then
-	( cd librumprun_unwind && ${RUMPMAKE} dependall && ${RUMPMAKE} install )
+	( cd librumprun_unwind &&
+		${RUMPMAKE} dependall &&
+		${RUMPMAKE} install
+	) || die librumprun_unwind build failed
 fi
 
 makekernlib ()
@@ -31,18 +34,18 @@ makekernlib ()
 	lib=$1
 	OBJS=`pwd`/rumpobj/$lib
 	mkdir -p ${OBJS}
-	( cd ${lib}
-		${RUMPMAKE} MAKEOBJDIRPREFIX=${OBJS} obj
-		${RUMPMAKE} MAKEOBJDIRPREFIX=${OBJS} dependall
+	( cd ${lib} &&
+		${RUMPMAKE} MAKEOBJDIRPREFIX=${OBJS} obj &&
+		${RUMPMAKE} MAKEOBJDIRPREFIX=${OBJS} dependall &&
 		${RUMPMAKE} MAKEOBJDIRPREFIX=${OBJS} install
-	)
+	) || die makekernlib $lib failed
 }
 make -C xen links
 makekernlib rumpxenif
 makekernlib rumpxendev
 
 # build the domU image
-make CONFIG_CXX="${BUILDRUMP_CXX}"
+make CONFIG_CXX="${BUILDRUMP_CXX}" || die make failed
 
 echo
 echo ">> $0 ran successfully"
