3a61ed876cf2123118fca14c2c8a8ea06cfe2edf
Antti Kantee <pooka@iki.fi>
2015-03-03 19:56:52 +0000

    Move files into platform/baremetal
    Prepares for the rumprun merge.

---
+++ build-rr.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+die ()
+{
+
+	echo '>>' $*
+	exit 1
+}
+
+[ $# -gt 0 ] || die Must give platform as first argument!
+platform=$1
+shift
+
+case ${platform} in
+'baremetal')
+	script=platform/baremetal/buildme.sh
+	result=platform/baremetal/rump
+	;;
+*)
+	die Platform \"$platform\" not supported!
+	;;
+esac
+
+export BUILDRUMP=$(pwd)/buildrump.sh
+export RUMPSRC=$(pwd)/rumpsrc
+( cd $(dirname ${script}) && ./$(basename ${script}) "$@" )
+[ $? -eq 0 ] || die Build script \"$script\" failed!
+
+ln -s ${result} ./rump
