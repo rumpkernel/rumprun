8b3032be2a251829a673e9b17148eea6cc06cf5b
Antti Kantee <pooka@iki.fi>
2015-03-18 00:26:54 +0000

    fix tyop

--- platform/xen/buildxen.sh
+++ platform/xen/buildxen.sh
@@ -25,7 +25,7 @@ if [ ! -f ${BUILDRUMP}/subr.sh ]; then
 	)
 fi
 . ${BUILDRUMP}/subr.sh
-../../_build-common.sh.sh "$@" || die _build-common.sh failed
+../../_build-common.sh "$@" || die _build-common.sh failed
 
 RUMPMAKE=$(pwd)/rumptools/rumpmake
 export RUMPMAKE
