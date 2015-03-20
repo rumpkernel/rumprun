237dd2b8df6ecb9bace14d0d0c915c3d1d198db8
Antti Kantee <pooka@iki.fi>
2015-03-20 12:20:40 +0000

    collect some universal build flags into one place: global.mk

---
+++ global.mk
@@ -0,0 +1,8 @@
+DBG?=	 -O2 -g
+CFLAGS+= -std=gnu99 ${DBG}
+CFLAGS+= -fno-stack-protector -ffreestanding
+
+CFLAGS+= -Wall -Wimplicit -Wmissing-prototypes -Wstrict-prototypes
+ifndef NOGCCERROR
+CFLAGS+= -Werror
+endif
