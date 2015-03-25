627f2ee4597a6f75244f4c1a90f72fac8a7cf632
Antti Kantee <pooka@iki.fi>
2015-03-25 10:17:52 +0000

    unify libc-level malloc wrappers to rumprun_base

--- lib/librumprun_base/malloc.c
+++ lib/librumprun_base/malloc.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <mini-os/os.h>
-#include <mini-os/mm.h>
-
+#include <bmk-core/errno.h>
 #include <bmk-core/memalloc.h>
 
 int
 posix_memalign(void **rv, size_t nbytes, size_t align)
 {
 	void *v;
-	int error = 10; /* XXX */
+	int error = BMK_ENOMEM;
 
 	if ((v = bmk_memalloc(nbytes, align)) != NULL) {
 		*rv = v;
@@ -30,17 +28,8 @@ malloc(size_t size)
 void *
 calloc(size_t n, size_t size)
 {
-	void *v;
-	size_t tot = n * size;
 
-	if (size != 0 && tot / size != n)
-		return NULL;
-
-	if ((v = malloc(tot)) != NULL) {
-		memset(v, 0, tot);
-	}
-
-	return v;
+	return bmk_memcalloc(n, size);
 }
 
 void *
