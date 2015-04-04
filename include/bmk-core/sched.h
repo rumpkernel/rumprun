908eaed9b71b5e53b8958a709cdd724dc7561f3b
Antti Kantee <pooka@iki.fi>
2015-04-04 15:23:34 +0000

    Move the baremetal scheduler interface definition to bmk-core.
    The implementations for baremetal and mini-os still remain private
    (for now).

--- include/bmk-core/sched.h
+++ include/bmk-core/sched.h
@@ -9,4 +9,38 @@ struct bmk_tcb {
 	unsigned long btcb_tpsize;	/* tls area length	*/
 };
 
+struct bmk_thread;
+
+void	bmk_sched_init(void);
+void	bmk_sched(void);
+
+struct bmk_thread *bmk_sched_create(const char *, void *, int,
+				    void (*)(void *), void *,
+				    void *, unsigned long);
+void	bmk_sched_join(struct bmk_thread *);
+void	bmk_sched_exit(void) __attribute__((__noreturn__));
+
+void	bmk_sched_block(struct bmk_thread *);
+void	bmk_sched_block_timeout(struct bmk_thread *, bmk_time_t);
+
+void	bmk_sched_wake(struct bmk_thread *);
+void	bmk_sched_setwakeup(struct bmk_thread *, bmk_time_t);
+
+int	bmk_sched_nanosleep(bmk_time_t);
+int	bmk_sched_nanosleep_abstime(bmk_time_t);
+
+void	*bmk_sched_gettls(struct bmk_thread *, unsigned int);
+void	bmk_sched_settls(struct bmk_thread *, unsigned int, void *);
+
+void	bmk_cpu_sched_create(struct bmk_tcb *,
+			     void (*)(void *), void *,
+			     void *, unsigned long);
+void	bmk_cpu_sched_switch(struct bmk_tcb *, struct bmk_tcb *);
+
+void	bmk_sched_set_hook(void (*)(void *, void *));
+struct bmk_thread *bmk_sched_init_mainlwp(void *);
+
+struct bmk_thread *bmk_sched_current(void);
+int *bmk_sched_geterrno(void);
+
 #endif /* _BMK_CORE_SCHED_H_ */
