31d687661f2b05d412f0fe4320759d43e30b11fc
Antti Kantee <pooka@iki.fi>
2015-04-05 14:40:33 +0000

    push common rumpuser hypercall stubs to bmk-core

--- platform/baremetal/undefs.c
+++ platform/baremetal/undefs.c
@@ -11,23 +11,12 @@
 #define REALNOTHING(name) \
     int name(void); int name(void) {return 1;}
 
-NOTHING(rumpuser_anonmmap)
-
-REALNOTHING(rumpuser_dl_bootstrap)
-
-NOTHING(rumpuser_daemonize_begin)
-NOTHING(rumpuser_daemonize_done)
 NOTHING(rumpuser_exit)
-NOTHING(rumpuser_kill)
 
 NOTHING(rumpuser_open)
 NOTHING(rumpuser_close)
 REALNOTHING(rumpuser_getfileinfo)
 NOTHING(rumpuser_bio)
-NOTHING(rumpuser_iovread)
-NOTHING(rumpuser_iovwrite)
-
-NOTHING(rumpuser_unmap)
 
 /* libc */
 
