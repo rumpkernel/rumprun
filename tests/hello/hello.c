1eecb172c3982e5e01f4b2e11a0990a1690237dd
Antti Kantee <pooka@iki.fi>
2015-04-07 15:32:59 +0000

    print walltime of day in greeting

--- tests/hello/hello.c
+++ tests/hello/hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #if defined(__linux__) || !defined(__NetBSD__)
 # error compiler wrapper fail
@@ -8,11 +9,17 @@
 int main (int argc, char *argv[])
 {
 	char *world = getenv("WORLD");
+	char buf[64];
+	time_t now;
 
 	if (world)
 		printf ("Hello, %s!\n", world);
 	else
 		printf ("Hello, world!\n");
+
+	now = time(NULL);
+	printf("When you do not hear the beep the time will be exactly:\n%s",
+	    ctime(&now));
 
 	printf("Sleeping 5s...\n");
 	sleep(5);
