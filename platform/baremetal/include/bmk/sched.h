ed1baa7154eec78819b786274bc06902df1c6987
Antti Kantee <pooka@iki.fi>
2015-04-18 16:27:36 +0000

    Define stack size in terms of 2^n multiple of page size.
    Not that it makes much difference now, since a page is just a value.
    But it gets us closer to what /xen is doing.

--- platform/baremetal/include/bmk/sched.h
+++ platform/baremetal/include/bmk/sched.h
@@ -1,6 +1,7 @@
 #include <bmk-core/types.h>
 #include <bmk-core/sched.h>
 
-#define BMK_THREAD_STACKSIZE (1<<16)
+#define BMK_THREAD_STACK_PAGE_ORDER 4
+#define BMK_THREAD_STACKSIZE ((1<<BMK_THREAD_STACK_PAGE_ORDER) * PAGE_SIZE)
 
 bmk_time_t bmk_cpu_clock_now(void);
