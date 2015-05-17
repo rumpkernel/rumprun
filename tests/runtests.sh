f19409e6e2ab245fc8125e9f465dec67aeed7af9
Antti Kantee <pooka@iki.fi>
2015-05-17 21:25:51 +0000

    specify OPT_SUDO in the right place
    from Justin

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -74,7 +74,7 @@ runguest ()
 	# img2=$3
 
 	[ -n "${img1}" ] || die runtest without a disk image
-	cookie=$(${RUMPRUN} ${STACK} ${OPT_SUDO} -b ${img1} ${testprog} __test)
+	cookie=$(${RUMPRUN} ${OPT_SUDO} ${STACK} -b ${img1} ${testprog} __test)
 	if [ $? -ne 0 -o -z "${cookie}" ]; then
 		TEST_RESULT=ERROR
 		TEST_ECODE=-2
