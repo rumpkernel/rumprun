c66305fb8fda545c09508fc958030e33e9d85819
Antti Kantee <pooka@iki.fi>
2015-05-19 13:42:39 +0000

    remove old example files.  support now in the rumprun launcher.

--- platform/baremetal/mkbootableiso.sh
+++
@@ -1,9 +0,0 @@
-[ $# -eq 0 ] && { echo give kernel name! ; exit 1 ; }
-
-basedir="/tmp/mkbootable$$"
-
-mkdir -p ${basedir}/boot/grub
-printf 'menuentry "rumpkernel" {\n\tmultiboot /boot/%s\n}\n' $1 \
-    > ${basedir}/boot/grub/grub.cfg
-ln -f $1 ${basedir}/boot
-grub-mkrescue -o $1.iso ${basedir}
