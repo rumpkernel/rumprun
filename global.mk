e1959a6905c20c39a6e3bb93acf5cc34aff7b9ec
Antti Kantee <pooka@iki.fi>
2015-05-26 23:13:15 +0000

    disable stack-protector for CXXFLAGS as well

--- global.mk
+++ global.mk
@@ -1,6 +1,7 @@
 DBG?=	 -O2 -g
 CFLAGS+= -std=gnu99 ${DBG}
 CFLAGS+= -fno-stack-protector -ffreestanding
+CXXFLAGS+= -fno-stack-protector -ffreestanding
 
 CFLAGS+= -Wall -Wimplicit -Wmissing-prototypes -Wstrict-prototypes
 ifndef NOGCCERROR
