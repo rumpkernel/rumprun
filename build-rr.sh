db752a82886e0820ff0041d6b4ef9368e66e48ea
Antti Kantee <pooka@iki.fi>
2015-05-27 22:00:04 +0000

    include _build-common.sh in build-rr.sh and remove former

--- build-rr.sh
+++ build-rr.sh
@@ -7,7 +7,12 @@ die ()
 	exit 1
 }
 
+set -e
+
+# defaults
+STDJ='-j4'
 RUMPSRC=src-netbsd
+
 while getopts '?qs:' opt; do
 	case "$opt" in
 	's')
@@ -59,7 +64,90 @@ fi
 . ${BUILDRUMP}/subr.sh
 
 . ${PLATFORMDIR}/platform.conf
-./_build-common.sh -V RUMP_CURLWP=__thread "$@" || die _build-common.sh failed
+
+rumptools=${PLATFORMDIR}/rumptools
+rumpobj=${PLATFORMDIR}/rumpobj
+rumpdest=${PLATFORMDIR}/rump
+
+# old git versions need to run submodule in the repo root. *sheesh*
+# We assume that if the git submodule command fails, it's because
+# we're using external $RUMPSRC
+(
+	cd $(git rev-parse --show-cdup)
+	if git submodule status ${RUMPSRC} 2>/dev/null | grep -q '^-' ; then
+		echo '>>'
+		echo '>> src-netbsd missing.  run "git submodule update --init"'
+		echo '>>'
+		exit 1
+	fi
+	if git submodule status ${RUMPSRC} 2>/dev/null | grep -q '^+' ; then
+		echo '>>'
+		echo '>> Your git submodules are out-of-date'
+		echo '>> Did you forget to run "git submodule update"?'
+		echo '>> (sleeping for 5s)'
+		echo '>>'
+		sleep 5
+	fi
+)
+
+# build tools
+${BUILDRUMP}/buildrump.sh ${BUILD_QUIET} ${STDJ} -k \
+    -V MKPIC=no -s ${RUMPSRC} -T ${rumptools} -o ${rumpobj} -d ${rumpdest} \
+    -V RUMP_CURLWP=__thread \
+    -V RUMP_KERNEL_IS_LIBC=1 -V BUILDRUMP_SYSROOT=yes "$@" tools
+
+[ -n "${BUILDXENMETAL_MKCONF}" ] \
+    && echo "${BUILDXENMETAL_MKCONF}" >> ${rumptools}/mk.conf
+
+RUMPMAKE=$(pwd)/${rumptools}/rumpmake
+MACHINE=$(${RUMPMAKE} -f /dev/null -V '${MACHINE}')
+[ -z "${MACHINE}" ] && die could not figure out target machine
+
+# build rump kernel
+${BUILDRUMP}/buildrump.sh ${BUILD_QUIET} ${STDJ} -k \
+    -V MKPIC=no -s ${RUMPSRC} -T ${rumptools} -o ${rumpobj} -d ${rumpdest} \
+    -V RUMP_CURLWP=__thread \
+    -V RUMP_KERNEL_IS_LIBC=1 -V BUILDRUMP_SYSROOT=yes "$@" \
+    build kernelheaders install
+
+LIBS="$(stdlibs ${RUMPSRC}) $(pwd)/lib/librumprun_tester"
+if [ "$(${RUMPMAKE} -f ${rumptools}/mk.conf -V '${_BUILDRUMP_CXX}')" = 'yes' ]
+then
+	LIBS="${LIBS} $(stdlibsxx ${RUMPSRC})"
+fi
+
+usermtree ${rumpdest}
+userincludes ${RUMPSRC} ${LIBS}
+
+for lib in ${LIBS}; do
+	makeuserlib ${lib}
+done
+
+eval ${BUILDXENMETAL_PCI_P} && makepci ${RUMPSRC} ${BUILDXENMETAL_PCI_ARGS}
+
+# build unwind bits if we support c++
+if havecxx; then
+        ( cd lib/librumprun_unwind
+	    ${RUMPMAKE} dependall && ${RUMPMAKE} install )
+        CONFIG_CXX=yes
+else
+        CONFIG_CXX=no
+fi
+
+configmk=${PLATFORMDIR}/config.mk
+
+echo "BUILDRUMP=${BUILDRUMP}" > ${configmk}
+echo "RUMPSRC=${RUMPSRC}" >> ${configmk}
+echo "CONFIG_CXX=${CONFIG_CXX}" >> ${configmk}
+echo "RUMPMAKE=${RUMPMAKE}" >> ${configmk}
+echo "BUILDRUMP_TOOLFLAGS=$(pwd)/${rumptools}/toolchain-conf.mk" >> ${configmk}
+echo "MACHINE=${MACHINE}" >> ${configmk}
+
+tools="AR CPP CC INSTALL NM OBJCOPY"
+havecxx && tools="${tools} CXX"
+for t in ${tools}; do
+	echo "${t}=$(${RUMPMAKE} -f bsd.own.mk -V "\${${t}}")" >> ${configmk}
+done
 
 export RUMPMAKE=$(pwd)/${PLATFORMDIR}/rumptools/rumpmake
 doextras || die 'platforms extras failed.  tillerman needs tea?'
@@ -71,4 +159,5 @@ ln -sf ${PLATFORMDIR}/rump .
 
 echo
 echo ">> $0 ran successfully"
+
 exit 0
