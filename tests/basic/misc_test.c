8cecfae0d76005d1f0de61c903d9f42e6a944243
Antti Kantee <pooka@iki.fi>
2015-06-23 11:37:52 +0000

    Check that pthread_mutex_init() works from ctor.
    Based on problem report and test case from @liuw

--- tests/basic/misc_test.c
+++ tests/basic/misc_test.c
@@ -1,5 +1,6 @@
 #include <sys/times.h>
 
+#include <pthread.h>
 #include <stdio.h>
 
 #include <rumprun/tester.h>
@@ -21,12 +22,35 @@ static int
 test_times(void)
 {
 	struct tms tms;
-	
+
 	printf("checking that calling times() does not crash ... ");
 	times(&tms);
 	prfres(0);
 
 	return 0;
+}
+
+/*
+ * Check that pthread has been initialized when constructors are
+ * called.  Issue reported by @liuw on irc
+ */
+static pthread_mutex_t ptm;
+static void __attribute__((constructor))
+pthmtxinit(void)
+{
+
+	pthread_mutex_init(&ptm, NULL);
+}
+static int
+test_pthread_in_ctor(void)
+{
+	int rv;
+
+	printf("checking that pthread_mutex_init() works from ctor ... ");
+	rv = pthread_mutex_destroy(&ptm);
+	prfres(rv);
+
+	return rv;
 }
 
 int
@@ -35,6 +59,7 @@ rumprun_test(int argc, char *argv[])
 	int rv = 0;
 
 	rv += test_times();
+	rv += test_pthread_in_ctor();
 
 	return rv;
 }
