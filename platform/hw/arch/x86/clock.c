7eade13a118e3c68822a30f3173ffe6cde36c977
Martin Lucina <martin@lucina.net>
2015-06-26 17:50:00 +0200

    hw: Dynamically program PIT in one-shot mode when idle
    Instead of running the clock interrupt with a fixed periodic tick when
    idle, dynamically program the PIT in one-shot mode to fire a single
    interrupt after the requested delay has elapsed.

--- platform/hw/arch/x86/clock.c
+++ platform/hw/arch/x86/clock.c
@@ -28,6 +28,8 @@
 
 #define NSEC_PER_SEC	1000000000ULL
 #define TSC_SHIFT	27
+#define PIT_DIVISOR	((NSEC_PER_SEC << 32) / TIMER_HZ)
+#define PIT_MIN_DELTA	16
 
 /* clock isr trampoline (in locore.S) */
 void bmk_cpu_isr_clock(void);
@@ -38,6 +40,9 @@ static uint64_t tsc_mult;
 /* Base time values at the last call to bmk_cpu_clock_now(). */
 static bmk_time_t time_base;
 static uint64_t tsc_base;
+
+/* Set to 1 when the i8254 interrupt is handled. */
+static volatile int ticktock = 0;
 
 /*
  * Read the current i8254 channel 0 tick count.
@@ -94,13 +99,6 @@ bmk_x86_initclocks(void)
 {
 	uint64_t tsc_freq;
 
-	/*
-	 * map clock interrupt.
-	 * note, it's still disabled in the PIC, we only enable it
-	 * during nanohlt
-	 */
-	bmk_x86_fillgate(32, bmk_cpu_isr_clock, 0);
-
 	/* Initialise i8254 timer channel 0 to mode 2 at 100 Hz */
 	outb(TIMER_MODE, TIMER_SEL0 | TIMER_RATEGEN | TIMER_16BIT);
 	outb(TIMER_CNTR, (TIMER_HZ / HZ) & 0xff);
@@ -115,10 +113,22 @@ bmk_x86_initclocks(void)
 	tsc_freq = (rdtsc() - tsc_base) * 10;
 
 	/*
+	 * Reinitialise i8254 timer channel 0 to mode 4 (one shot).
+	 */
+	outb(TIMER_MODE, TIMER_SEL0 | TIMER_ONESHOT | TIMER_16BIT);
+
+	/*
 	 * Calculate TSC scaling multiplier and initialiase time_base.
 	 */
 	tsc_mult = (NSEC_PER_SEC << TSC_SHIFT) / tsc_freq;
 	time_base = (tsc_base * tsc_mult) >> TSC_SHIFT;
+
+	/*
+	 * Map i8254 interrupt vector and enable it in the PIC.
+	 * XXX: We don't really want to enable IRQ2 here, but ...
+	 */
+	bmk_x86_fillgate(32, bmk_cpu_isr_clock, 0);
+	outb(PIC1_DATA, 0xff & ~(1<<2|1<<0));
 }
 
 void
@@ -126,9 +136,10 @@ bmk_isr_clock(void)
 {
 
 	/*
-	 * Nothing to do here, clock interrupt serves only as a way to wake
-	 * the CPU from halt state.
+	 * Nothing to do here except signal completion, the clock interrupt
+	 * serves only as a way to wake the CPU from halt state.
 	 */
+	ticktock = 1;
 }
 
 bmk_time_t
@@ -153,18 +164,48 @@ bmk_cpu_clock_now(void)
 void
 bmk_cpu_block(bmk_time_t until)
 {
+	bmk_time_t now, delta_ns;
+	int64_t delta_ticks;
+	unsigned int ticks;
 
-	bmk_cpu_nanohlt();
-}
+	now = bmk_cpu_clock_now();
+	if (until < now)
+		return;
 
-void
-bmk_cpu_nanohlt(void)
-{
+	delta_ns = until - now;
+	delta_ticks = (delta_ns << 32) / PIT_DIVISOR;
 
 	/*
-	 * Enable clock interrupt and wait for the next whichever interrupt.
+	 * While the delay is less than a minimum sane amount of ticks,
+	 * loop and program the timer to interrupt the CPU after the
+	 * delay has expired.
 	 */
-	outb(PIC1_DATA, 0xff & ~(1<<2|1<<0));
-	hlt();
-	outb(PIC1_DATA, 0xff & ~(1<<2));
+	while (delta_ticks >= PIT_MIN_DELTA) {
+		/*
+		 * Maximum timer delay is 65535 ticks.
+		 */
+		if (delta_ticks > 65535)
+			ticks = 65535;
+		else
+			ticks = delta_ticks;
+
+		/*
+		 * Note that according to the Intel 82C54 datasheet, p12 the
+		 * interrupt is actually delivered in N + 1 ticks.
+		 */
+		outb(TIMER_CNTR, (ticks - 1) & 0xff);
+		outb(TIMER_CNTR, (ticks - 1) >> 8);
+
+		/*
+		 * Wait for any interrupt. If we got a non-timer interrupt then
+		 * just return into the scheduler which will check if there is
+		 * work to do.
+		 */
+		hlt();
+		if(!ticktock)
+			break;
+		ticktock = 0;
+
+		delta_ticks -= ticks;
+	}
 }
