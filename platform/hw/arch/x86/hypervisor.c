eb4d9a8c9d28036bf3612f8a874abd3ed94c09b5
Martin Lucina <martin@lucina.net>
2015-10-01 17:34:07 +0200

    Add function to detect if running under hypervisor

---
+++ platform/hw/arch/x86/hypervisor.c
@@ -0,0 +1,66 @@
+/*-
+ * Copyright (c) 2015 Martin Lucina.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <hw/types.h>
+#include <hw/kernel.h>
+
+#include <arch/x86/hypervisor.h>
+
+int hypervisor_detect(void)
+{
+	uint32_t eax, ebx, ecx, edx;
+
+	/*
+	 * First check for generic "hypervisor present" bit.
+	 */
+	x86_cpuid(0x0, &eax, &ebx, &ecx, &edx);
+	if (eax >= 0x1) {
+		x86_cpuid(0x1, &eax, &ebx, &ecx, &edx);
+		if (!(ecx & (1 << 31)))
+			return 0;
+	}
+	else
+		return 0;
+
+	/*
+	 * CPUID leaf at 0x40000000 returns hypervisor vendor signature.
+	 * Source: https://lkml.org/lkml/2008/10/1/246
+	 */
+	x86_cpuid(0x40000000, &eax, &ebx, &ecx, &edx);
+	/* Xen: "XenVMMXenVMM" */
+	if (ebx == 0x566e6558 && ecx == 0x65584d4d && edx == 0x4d4d566e)
+		return HYPERVISOR_XEN;
+	/* VMware: "VMwareVMware" */
+	else if (ebx == 0x564d7761 && ecx == 0x7265564d && edx == 0x77617265)
+		return HYPERVISOR_VMWARE;
+	/* Hyper-V: "Microsoft Hv" */
+	else if (ebx == 0x4d696372 && ecx == 0x6f736f66 && edx == 0x74204876)
+		return HYPERVISOR_HYPERV;
+	/* KVM: "KVMKVMKVM\0\0\0" */
+	else if (ebx == 0x4b564d4b && ecx == 0x564d4b56 && edx == 0x4d000000)
+		return HYPERVISOR_KVM;
+
+	return 0;
+}
---
+++ platform/hw/include/arch/x86/hypervisor.h
@@ -0,0 +1,6 @@
+int hypervisor_detect(void);
+
+#define HYPERVISOR_XEN 1
+#define HYPERVISOR_VMWARE 2
+#define HYPERVISOR_HYPERV 3
+#define HYPERVISOR_KVM 4
