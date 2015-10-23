969c83f3e4e0301e18db9712f0731a4bbb22f121
Antti Kantee <pooka@iki.fi>
2015-10-23 14:20:47 +0000

    Source the conf before we (potentially) chdir.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -25,12 +25,12 @@
 # SUCH DAMAGE.
 #
 
-cd $(dirname $0) || die 'could not enter test dir'
-
 [ -n "${RUMPRUN_SHCONF}" ] || { echo '>> need RUMPRUN_SHCONF'; exit 1; }
 . "${RUMPRUN_SHCONF}"
 
 export PATH="${PATH}:${RRDEST}/bin"
+
+cd $(dirname $0) || die 'could not enter test dir'
 
 # we know, we knooooooow
 export RUMPRUN_WARNING_STFU=please
